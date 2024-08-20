57b7ee05fcac114af8afbdcf8fe4d09316bf22c0
Nastaran <nstrnm4381@gmail.com>
2024-08-20 23:36:55 +0330

    Add methods to retrieve books by various criteria in BookRepository

--- src/repository/BookRepository.java
+++ src/repository/BookRepository.java
@@ -1,8 +1,12 @@
 package repository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import model.Book;
+import enums.BookGenre;
+import java.util.stream.Collectors;
+
 
 public class BookRepository {
     private final Map<String, Book> books = new HashMap<>();
@@ -15,8 +19,35 @@ public class BookRepository {
         books.remove(book.getISBN());
     }
 
+    // Find books by ISBN
+    public Book getBookByISBN(String isbn) {
+        return books.get(isbn);
+    }
 
+    // Find books by author
+    public List<Book> getBooksByAuthor(String author) {
+        return books.values().stream()
+                .filter(book -> book.getAuthor().equalsIgnoreCase(author))
+                .collect(Collectors.toList());
+    }
 
+    // Find books by genre
+    public List<Book> getBooksByGenre(BookGenre genre) {
+        return books.values().stream()
+                .filter(book -> book.getGenre() == genre)
+                .collect(Collectors.toList());
+    }
 
+    // Find books by language
+    public List<Book> getBooksByLanguage(String language) {
+        return books.values().stream()
+                .filter(book -> book.getLanguage().equalsIgnoreCase(language))
+                .collect(Collectors.toList());
+    }
+
+    // Get all books in the repository
+    public List<Book> getAllBooks() {
+        return List.copyOf(books.values());
+    }
 
 }
