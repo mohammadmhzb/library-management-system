61433852df101185f5fbe892633012511113fb76
Iman <imanshans@gmail.com>
2024-08-27 15:10:49 +0330

    feat(model): Reservation model implemented

--- src/main/java/com/example/Librarymanagementsystem/model/Reservation.java
+++ src/main/java/com/example/Librarymanagementsystem/model/Reservation.java
@@ -1,52 +1,34 @@
 package com.example.Librarymanagementsystem.model;
 
-import com.example.Librarymanagementsystem.model.Book;
-import com.example.Librarymanagementsystem.model.User;
 import com.example.Librarymanagementsystem.model.enums.ReservationStatus;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "reservations")
 public class Reservation {
-    private static int nextId = 1;
-    private final int reservationId;
-    private final Book book;
-    private final User user;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "book_id", nullable = false)
+    private  Book book;
+
+    @Getter
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Getter
+    @Setter
     private ReservationStatus status;
 
-    public int getReservationId() {
-        return reservationId;
-    }
-
-    public Reservation(User user, Book book) {
-        this.reservationId = nextId++;
-        this.book = book;
-        this.user = user;
-        this.status = ReservationStatus.PENDING;
-    }
-
-
-    public Book getBook() {
-        return book;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public ReservationStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ReservationStatus status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "Reservation{" +
-                " book=" + book +
-                ", user=" + user +
-                ", status=" + status +
-                '}';
-    }
 }
 
