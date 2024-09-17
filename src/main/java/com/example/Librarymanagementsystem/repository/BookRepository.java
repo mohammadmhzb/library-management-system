83c2922ca4dd95e5fea78a726030bd5acec824ca
Nastaran <nstrnm4381@gmail.com>
2024-09-18 00:34:04 +0330

    feat: add BookService and BookRepository for managing book operations

--- src/main/java/com/example/Librarymanagementsystem/repository/BookRepository.java
+++ src/main/java/com/example/Librarymanagementsystem/repository/BookRepository.java
@@ -1,51 +1,32 @@
 package com.example.Librarymanagementsystem.repository;
-//
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.stream.Collectors;
-//
-//
-public class BookRepository {
-//    private final Map<String, Book> books = new HashMap<>();
-//
-//    public void addBook(Book book) {
-//        books.put(book.getISBN(), book);
-//    }
-//
-//    public void removeBook(Book book) {
-//        books.remove(book.getISBN());
-//    }
-//
-//    // Find books by ISBN
-//    public Book getBookByISBN(String isbn) {
-//        return books.get(isbn);
-//    }
-//
-//    // Find books by author
-//    public List<Book> getBooksByAuthor(String author) {
-//        return books.values().stream()
-//                .filter(book -> book.getAuthor().equalsIgnoreCase(author))
-//                .collect(Collectors.toList());
-//    }
-//
-//    // Find books by genre
-//    public List<Book> getBooksByGenre(BookGenre genre) {
-//        return books.values().stream()
-//                .filter(book -> book.getGenre() == genre)
-//                .collect(Collectors.toList());
-//    }
-//
-//    // Find books by language
-//    public List<Book> getBooksByLanguage(String language) {
-//        return books.values().stream()
-//                .filter(book -> book.getLanguage().equalsIgnoreCase(language))
-//                .collect(Collectors.toList());
-//    }
-//
-//    // Get all books in the repository
-//    public List<Book> getAllBooks() {
-//        return List.copyOf(books.values());
-//    }
-//
+
+import com.example.Librarymanagementsystem.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Book findByTitle(String title);
+
+    Optional<Book> findById(Long id);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitleContaining(String title);
+
+    List<Book> findByAuthorContaining(String author);
+
+    List<Book> findByTitleStartingWith(String title);
+
+    List<Book> findByTitleEndingWith(String title);
+
+    List<Book> findByTitleContainingAndAuthorContaining(String title, String author);
+
+    List<Book> findByAuthorContainingAndTitleContaining(String author, String title);
+
+    List<Book> findByLanguage(String language);
 }
