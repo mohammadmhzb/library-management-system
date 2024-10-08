1c22d9140a67f9ce1acc0a501e15a29cafab9e72
Nastaran <nstrnm4381@gmail.com>
2024-10-09 00:55:22 +0330

    feat(exception): add GlobalExceptionHandler for centralized exception handling
    -Includes handlers for ResourceNotFoundException, DataAccessException, and general exceptions.

--- src/main/java/com/example/Librarymanagementsystem/payload/response/Response.java
+++ src/main/java/com/example/Librarymanagementsystem/payload/response/Response.java
@@ -6,13 +6,13 @@ import java.time.LocalDateTime;
 
 @Setter
 @Getter
-public class Response {
+public class Response<T> {
 
     private LocalDateTime timestamp;
     private int status;
-    private String message;
+    private T message;
 
-    public Response(LocalDateTime timestamp, int status, String message) {
+    public Response(LocalDateTime timestamp, int status, T message) {
         this.timestamp = timestamp;
         this.status = status;
         this.message = message;
