c15a58569ab0e663fdcb11d4b5f86e970b49eefe
Nastaran <nstrnm4381@gmail.com>
2024-10-12 10:46:57 +0330

    feat(dto): add UserResponseDTO for user data responses
    - Implements fields for firstName, lastName, phoneNumber, and email
    - Designed to structure response data when fetching user information

---
+++ src/main/java/com/example/Librarymanagementsystem/payload/response/UserResponseDTO.java
@@ -0,0 +1,12 @@
+package com.example.Librarymanagementsystem.payload.response;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDTO {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+}
